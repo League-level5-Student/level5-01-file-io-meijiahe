c177ed78753e42de0c9b8458e63a24b92b0b9b9e
Meijia He <hemeijia0602@gmail.com>
2022-05-28 11:26:18 -0700

    File Recorder not finished, in progress of working on the file.

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,11 @@
 package _01_File_Recorder;
 
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog(null,"Please enter a message in the box.");
+		
+	}
 }
